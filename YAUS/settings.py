053fd0f80ae39e8a4fd9397f913620d32f76d1a5
Andres Cendales <andres01660@gmail.com>
2020-08-26 21:35:33 -0500

    save only random part of shor_ur

--- YAUS/settings.py
+++ YAUS/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ndds9!(0@qr!g^8$4ifcbnyiaumlx^(1q!62ko#s6d12ts2eqj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '*'
 ]
