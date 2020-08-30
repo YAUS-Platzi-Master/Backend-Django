c76851636ec9bdf06079171791f4b2b3f48f385c
Andres Cendales <andres01660@gmail.com>
2020-08-30 11:49:43 -0500

    add csrf excempt

--- authKnox/views.py
+++ authKnox/views.py
@@ -55,7 +55,7 @@ class ListTokenProfileView(APIView):
         return Response(data=data,status=status.HTTP_200_OK)
 
 
-@method_decorator(ensure_csrf_cookie,name='post')
+@method_decorator(csrf_exempt,name='post')
 class LoginView(LoginView):
     """Login View"""
     permission_classes = [AllowAny,]
