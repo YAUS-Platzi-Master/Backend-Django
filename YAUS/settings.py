7ed5392bbfa84c15f0f734fd14ffae7b86b28013
Andres Cendales <andres01660@gmail.com>
2020-08-29 21:02:38 -0500

    add settings middlewares cors

--- YAUS/settings.py
+++ YAUS/settings.py
@@ -91,10 +91,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-
-
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'YAUS.urls'
 
